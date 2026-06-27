d7de5aa8a71cdca7bdbf2ca44773c1fc14000f90
Rozer Bagh <40331263+rozerbagh@users.noreply.github.com>
2026-06-27 12:08:43 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+        const set = new Set();
+        for (let n of nums){
+            if(set.has(n)){
+                return true
+            }
+            set.add(n)
+        }
+        return false;
+    }
+}
